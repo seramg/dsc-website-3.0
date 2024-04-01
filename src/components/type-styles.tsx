b0fe9ef2323a0bf5c03872a5f0bb52bac8d551b7
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-04-01 11:41:57 +0530

    chore: add breakpoints, typestyle variants
    - Add breakpoints for devices
    - Add typestyle variants for mobile

--- src/components/type-styles.tsx
+++ src/components/type-styles.tsx
@@ -1,29 +1,38 @@
 import styled from "styled-components";
+import { device } from "./breakpoints";
 
 const Title = styled.h1`
   font-size: 64px;
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1.1;
   letter-spacing: -0.02em;
+
+  @media ${device.sm} {
+    font-size: 40px;
+  }
 `;
 
 const Heading1 = styled.h2`
   font-size: 48px;
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1.1;
   letter-spacing: -0.02em;
+
+  @media ${device.sm} {
+    font-size: 36px;
+  }
 `;
 
 const Heading2 = styled.h3`
   font-size: 32px;
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1.1;
   letter-spacing: -0.02em;
 `;
 
 const Heading3 = styled.h4`
   font-size: 24px;
-  font-weight: 700;
+  font-weight: 500;
   line-height: 1.2;
   letter-spacing: -0.02em;
 `;
@@ -33,6 +42,10 @@ const BodyLarge = styled.p`
   font-weight: 400;
   line-height: 1.4;
   letter-spacing: -0.02em;
+
+  @media ${device.sm} {
+    font-size: 20px;
+  }
 `;
 
 const Body = styled.p`
