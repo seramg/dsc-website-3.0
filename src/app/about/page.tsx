bfa548d00b0719803dad18df778341d8e266dec7
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-04-01 11:22:56 +0530

    chore: update theme utils, typestyles
    - Add theme toggle (test component)
    -  Add Standardised Colour Variables
    -  Add Styled Components for Typestyles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,33 @@
+"use client";
+
 import React from "react";
+
+import { ModeToggle } from "./test";
+
+import {
+  Title,
+  Heading1,
+  Heading2,
+  Heading3,
+  BodyLarge,
+  Body,
+  BodySmall,
+} from "./../../components/type-styles"; // Import the missing components
 
 const page = () => {
   return (
     <div>
-      <h1 className="bg-gdsccolours-primary900-green900">About</h1>
+      <h1>About</h1>
       <p>This is the about page</p>
+      <div className="bg-backgroundEmPrimary">This is a test</div>
+      <Title>Title</Title>
+      <Heading1>Heading 1</Heading1>
+      <Heading2>Heading 2</Heading2>
+      <Heading3>Heading 3</Heading3>
+      <BodyLarge>Body Large</BodyLarge>
+      <Body>Body</Body>
+      <BodySmall>Body Small</BodySmall>
+      <ModeToggle />
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,71 +19,19 @@ const config = {
     },
     extend: {
       colors: {
-        gdsccolours: {
-          lightgrey: {
-            100: "#f2f2f2",
-            500: "#e6e6d0",
-            900: "#e6e6e6",
-          },
-          neutralcore: {
-            white: "#ffffff",
-            black: "#000000",
-          },
-          darkgrey: {
-            100: "#cccccc",
-            200: "#838383",
-            300: "#999999",
-            500: "#808080",
-            600: "#666666",
-            700: "#4d4d4d",
-            800: "#333333",
-            900: "#1a1a1a",
-          },
-          primary: {
-            900: {
-              blue: "#2196f3",
-              green: "#4caf50",
-              red: "#f44336",
-              yellow: "#ffc107",
-            },
-            800: {
-              blue: "#42a5f5",
-              green: "#66bb6a",
-              red: "#ef5350",
-              yellow: "#ffca28",
-            },
-            500: {
-              blue: "#64b5f6",
-              green: "#81c784",
-              red: "#e57373",
-              yellow: "#ffd54f",
-            },
-            300: {
-              blue: "#90caf9",
-              green: "#a5d6a7",
-              red: "#ef9a9a",
-              yellow: "#ffe082",
-            },
-            200: {
-              blue: "#bbdefb",
-              green: "#c8e6c9",
-              red: "#ffcdd2",
-              yellow: "#ffecb3",
-            },
-            100: {
-              blue: "#e3f2fd",
-              green: "#e8f5e9",
-              red: "#ffebee",
-              yellow: "#fff8e1",
-            },
-            core: {
-              green: "#34a853",
-              blue: "#4285f4",
-              red: "#ea4335",
-              yellow: "#fbbc04",
-            },
-          },
-        },
+        
+        // Project Colours
+        onBackgroundPrimary: "var(--on-background-primary)",
+        onBackgroundSecondary: "var(--on-background-secondary)",
+
+        backgroundPrimary: "var(--background-primary)",
+        backgroundSecondary: "var(--background-secondary)",
+
+        backgroundEmPrimary: "var(--background-em-primary)",
+        backgroundEmSecondary: "var(--background-em-secondary)",
+        backgroundEmTertiary: "var(--background-em-tertiary)",
+
+        // Shadcn Colours
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
