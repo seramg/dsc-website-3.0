6757d8d4390f7bae9d053e967e83eba4d5e209a9
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-04-25 14:03:12 +0530

    fix(useResultantTheme hook): incorrect implementation
    - fix and implement useEffect to prevent client-server disparities.

--- src/components/hooks/useResultantTheme.ts
+++ src/components/hooks/useResultantTheme.ts
@@ -1,6 +1,15 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function useResultantTheme() {
     const { theme, systemTheme } = useTheme();
-    return theme === "system"? systemTheme : theme;
+    const [resultantTheme,setResultantTheme] = useState<string|undefined>("light");
+
+    useEffect(() => {
+        const result = theme === "system"? systemTheme : theme;
+        setResultantTheme(result);
+    }, [systemTheme, theme]);
+
+    return resultantTheme;
+   
 }
