37afd954b2f0a2a24e87ab6b10dcf52c219aa7c9
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-04-25 14:05:21 +0530

    chore(MarqueeContainer): update styling
    -make dynamic (seperate data from ui)
    - fix layout resize issues (add container over marquee)
    - update styling

--- src/components/MarqueeContainer.tsx
+++ src/components/MarqueeContainer.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Marquee from 'react-fast-marquee'
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const data = ["UXTopia", "Not just a regular design event", "Nov 15-21"];
 
 const MarqueeContainer = () => {
   return (
-    <Marquee
-        pauseOnHover
-        play
-        delay={0}
-        autoFill
-        className="h-8 bg-blue-500 text-white"
-      >
-        <span>UXTopia</span>
-        <div className=" justify-center rounded-full bg-white h-5 w-5 mx-8"></div>
-        <span>Not just a regular design event</span>
-        <div className=" justify-center rounded-full bg-white h-5 w-5 mx-8"></div>
-        <span>Nov 15-21</span>
-        <div className=" justify-center rounded-full bg-white h-5 w-5 mx-8"></div>
+    <div className="h-8 bg-onBackgroundEmPrimary text-backgroundEmPrimary flex justify-center items-center">
+      <Marquee pauseOnHover play delay={0} autoFill>
+        {data.map((item, index) => (
+          <div key={index} className="flex items-center justify-center">
+            <span>{item}</span>
+            <div className=" justify-center rounded-full bg-backgroundEmPrimary h-2 w-2 mx-4"></div>
+          </div>
+        ))}
       </Marquee>
-  )
-}
+    </div>
+  );
+};
 
-export default MarqueeContainer
+export default MarqueeContainer;
