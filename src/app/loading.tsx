5f50100773277b598b9cec5022d055560e59caa0
Navaneeth Venu <69888461+navaneethvenu@users.noreply.github.com>
2024-04-25 21:32:02 +0530

    fix: error in tw background class

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 bg-BackgroundPrimary z-[10000] flex flex-1 items-center justify-center">
+    <div className="fixed inset-0 bg-backgroundPrimary z-[10000] flex flex-1 items-center justify-center">
       <LoaderCircle className="text-onBackgroundPrimary animate-spin" />
     </div>
   );
